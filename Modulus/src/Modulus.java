972f7344d9669d85ca879463ef82b4918b15e61f
michael.brady <michael.brady108@gmail.com>
2016-08-25 09:08:35 -0600

    First 3 Exercises are done

--- Modulus/src/Modulus.java
+++ Modulus/src/Modulus.java
@@ -1,5 +1,51 @@
-
+import java.util.Scanner;
 public class Modulus
 	{
-
+		public static void main(String[] args)
+			{
+				//exercise1();
+				//exercise2();
+				exercise3();
+			}
+		public static void exercise1()
+			{
+				Scanner input = new Scanner(System.in);
+				System.out.println("Type a number");
+				int number = input.nextInt();
+				if(number%2 ==1)
+					{
+						System.out.println("The number is odd");
+					}
+				else
+					{
+						System.out.println("The number is even");
+					}
+				
+			}
+		public static void exercise2()
+		{
+			Scanner input2 = new Scanner(System.in);
+			System.out.println("Type in a year");
+			int year = input2.nextInt();
+			if(year%4 == 0)
+				{
+					System.out.println("This is a leap year");
+				}
+			else
+				{
+					System.out.println("This is not a leap year");
+				}
+		}
+		public static void exercise3()
+		{
+			int i = 0;
+			int[] array = {2, 5, -7, 1, 3, 12, 19, -16, 4, 20};
+			for (i = 0; i<array.length; i++)
+				{
+					if((i)%3 == 0)
+						{
+							System.out.println(array[i]);
+						}
+				}
+		}
 	}
